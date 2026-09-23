723933c59f38449407efcb82ffec5afb2b828d7c
Ryan <ryanmaulana03@guru.smk.belajar.id>
2026-09-23 10:32:05 +0700

    Update API URL to https://eling2.bisnisumkm.biz.id and change applicationId to com.eling2.pos_mobile

--- lib/core/api_config.dart
+++ lib/core/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'https://dimsum.bisnisumkm.biz.id';
+  static const String baseUrl = 'https://eling2.bisnisumkm.biz.id';
   // static const String baseUrl = 'http://10.0.2.2:8000'; // Untuk Emulator Android
   // static const String baseUrl = 'http://192.168.17.9:8000'; // IP Local Anda
   // static const String baseUrl = 'http://192.168.11.247:8000'; // IP Local Anda
